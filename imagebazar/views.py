2e2b42f5da6aa0e595bf89be5cdfdc859d55175c
myth-vivek <vivekmgr229@gmail.com>
2020-08-28 21:30:29 +0545

    first commit

---
+++ imagebazar/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+from myapp.models import *
+
+
+def show_about_page(request):
+    name = "vivek"
+    link = "vivek.com"
+
+    data = {
+        'name': name,
+        'link': link
+    }
+    return render(request, "about.html", data)
+
+
+def show_home_page(request):
+    cats = Category.objects.all()
+    images = Image.objects.all()
+    data = {'images': images, 'cats': cats}
+    return render(request, "home.html", data)
+
+
+def show_category_page(request, cid):
+    cats = Category.objects.all()
+    category = Category.objects.get(pk=cid)
+
+    images = Image.objects.filter(cat=category)
+
+    data = {'images': images, 'cats': cats}
+    return render(request, "home.html", data)
